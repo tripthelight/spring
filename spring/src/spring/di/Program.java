de595126b5a690b04a20d69d4517779a3c2fbb41
tripthelight <bye_ok@naver.com>
2024-05-01 10:56:02 +0900

    [240501] study

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,8 +1,13 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -16,9 +21,19 @@ public class Program {
 		console.setExam(exam);
 		*/
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		// ExamConsole console = (ExamConsole) context.getBean("console");
 		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		// List<Exam> exams = new ArrayList<>();
+		List<Exam> exams = (List<Exam>) context.getBean("exams");
+		// exams.add(new NewlecExam(1,1,1,1));
+		
+		for(Exam e : exams)
+			System.out.println(e);
 
 	}
 
